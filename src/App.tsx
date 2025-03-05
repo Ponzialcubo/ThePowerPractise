2eaebfaaa04d5818335873d33575113919e63f86
Ponzi <ponzialcubo@gmail.com>
2025-03-05 12:28:50 +0100

    FUNCIONANDO

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,53 @@
 import { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/cryptos'; // Dirección de tu servidor Node.js
-
-interface Crypto {
-  id: number;
+type Crypto = {
+  asset_id: string;
   name: string;
-  symbol: string;
-  quote: {
-    USD: {
-      price: number;
-    };
-  };
-}
+  type_is_crypto: number;
+  data_quote_start: string;
+  data_quote_end: string;
+  data_orderbook_start: string;
+  data_orderbook_end: string;
+  data_trade_start: string;
+  data_trade_end: string;
+  data_symbols_count: number;
+  volume_1hrs_usd: number;
+  volume_1day_usd: number;
+  volume_1mth_usd: number;
+  id_icon: string;
+  chain_addresses: {
+    chain_id: string;
+    network_id: string;
+    address: string;
+  }[];
+  data_start: string;
+  data_end: string;
+};
+
+let config = {
+  method: 'get',
+  maxBodyLength: Infinity,
+  url: `${import.meta.env.VITE_BASE_URL}/v1/assets?filter_asset_id=BTC;ETH`,
+  headers: { 
+    'Accept': 'text/plain', 
+    'X-CoinAPI-Key': import.meta.env.VITE_COIN_API_KEY
+  }
+};
 
 function App() {
   const [cryptos, setCryptos] = useState<Crypto[]>([]);
-
+  
   useEffect(() => {
-    fetch(API_URL) // No es necesario pasar la API key en el cliente
-      .then((response) => response.json())
-      .then((data) => setCryptos(data.data))
-      .catch((error) => console.error('Error al obtener los datos:', error));
+    axios.request(config)
+    .then((response) => {
+      setCryptos(response.data)
+  console.log(JSON.stringify(response.data));
+})
+.catch((error) => {
+  console.log(error);
+});
   }, []);
 
   return (
@@ -29,8 +55,8 @@ function App() {
       <h1>Lista de Criptomonedas</h1>
       <ul>
         {cryptos.map((crypto) => (
-          <li key={crypto.id}>
-            {crypto.name} ({crypto.symbol}): ${crypto.quote.USD.price.toFixed(2)}
+          <li key={crypto.asset_id}>
+            {crypto.name} ({crypto.asset_id})
           </li>
         ))}
       </ul>
